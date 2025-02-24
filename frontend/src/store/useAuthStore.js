b7f60a1926273ef690f6516996410d2038207bd1
mareim <mariemouni060@gmail.com>
2025-02-24 11:21:33 +0100

    signup set authUser commented

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -36,7 +36,7 @@ export const useAuthStore = create((set, get) => ({
     set({ isSigningUp: true });
     try {
       const res = await axiosInstance.post("/auth/signup", data);
-      set({ authUser: res.data });
+      // set({ authUser: res.data });
       toast.success("Account created successfully");
       // get().connectSocket();
     } catch (error) {
