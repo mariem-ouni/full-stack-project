b2bdb7b85412420e06a8030499c47c32292c9f12
mareim <mariemouni060@gmail.com>
2025-02-24 10:55:15 +0100

    first deployement push

--- backend/src/index.js
+++ backend/src/index.js
@@ -34,15 +34,15 @@ const __dirname = path.resolve();
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
-if(process.env.NODE_ENV === "production") {
-   app.use(express.static(path.join(__dirname, "/frontend/dist"))); 
-    app.get("*", (req, res)=>{
-      res.usedFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
 
     })
-    
+
 }
-    
+
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
